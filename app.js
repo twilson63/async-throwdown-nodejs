e2460ef782c020a835c9439cbec0dd2d2d6f8457
Tom Wilson <tom@jackhq.com>
2013-07-04 07:15:41 -0400

    add cluster and domains

--- app.js
+++ app.js
@@ -7,48 +7,83 @@ var s3 = knox.createClient(require('./config'));
 var ee = new (require('events').EventEmitter);
 var engine = require('engine.io');
 var uuid = require('uuid');
+var cluster = require('cluster');
 
-var server = http.createServer(function(req, res) {
+if (cluster.isMaster) {
+  cluster.fork();
+  cluster.fork();
+
+  cluster.on('disconnect', function(worker) {
+    console.error('disconnect!');
+    cluster.fork();
+  });
+} else {
+  var domain = require('domain');
+
+  var server = http.createServer(function(req, res) {
+    var d = domain.create();
+    d.on('error', function(err) {
+      console.log(err.code);
+      var killtimer = setTimeout(function() {
+        process.exit(1);
+      }, 30000);
+      killtimer.unref();
+      server.close();
+      cluster.worker.disconnect(); 
+      // res.statusCode = 500;
+      // res.setHeader('content-type', 'text/plain');
+      res.writeHead(500, {'content-type': 'text/plain'});
+      res.end('Oops, there was a problem!\n');
+    });
+    d.add(req);
+    d.add(res);
+    d.run(function() {
+      handleRequest(req, res);
+    });
+  });
+
+  server.listen(8000);
+
+  var io = engine.attach(server);
+
+  io.on('connection', function(socket) {
+    ee.on('progress', function(status) {
+      socket.send(JSON.stringify(status));
+    });
+  });
+}
+
+function handleRequest(req, res) {
   if (req.url == '/js/engine.io.js' && req.method.toLowerCase() == 'get') {
     res.writeHead(200, { 'content-type': 'text/javascript'});
     fs.createReadStream('./engine.io.js').pipe(res);
     return;
   }
   if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
-    console.time('upload-file');
+    //console.time('upload-file');
     // parse a file upload
     var form = new formidable.IncomingForm();
     form.uploadDir = './uploads';
     form.keepExtensions = true;
     var name = uuid.v1();
-    form.on('progress', function(bytesReceived, bytesExpected) {
-      var status = { 
-        name: name,
-        percent: parseInt(
-          (bytesReceived / bytesExpected) * 100, 10)
+    form.on('progress', uploadProgress);
+    function uploadProgress(bytesReceived, bytesExpected) {
+      var status = { name: name,
+        percent: parseInt((bytesReceived / bytesExpected) * 100, 10)
       };
       ee.emit('progress', status);
-    });
-
+    };
+    
     form.parse(req, function(err, fields, files) {
-      console.log(err);
-      console.log(files.upload.name);
-      console.log(files.upload.path);
-      
-      // need to upload file to s3
       s3.putFile(files.upload.path, files.upload.name, function(e, r) {
         // remove file from tmp dir
-        if (e) { 
-          console.log(e);
-          res.writeHead(500, {'content-type': 'text/plain'});
-          res.write('error uploading file:\n\n');
-          res.end(util.inspect({fields: fields, files: files}));
-        }
+        // if (e) { 
+        //   res.writeHead(500, {'content-type': 'text/plain'});
+        //   res.write('error uploading file:\n\n');
+        //   res.end(util.inspect({fields: fields, files: files}));
+        // }
         res.writeHead(200, {'content-type': 'text/html'});
-        //res.write('received upload:\n\n');
-        //res.end(files.upload.name);
         fs.createReadStream('./index.html').pipe(res);
-        console.timeEnd('upload-file');
       })
       .on('progress', function(status) {
         status.name = files.upload.name;
@@ -71,22 +106,12 @@ var server = http.createServer(function(req, res) {
   streamFile('/main.js', 'text/javascript');
   streamFile('/main.html', 'text/html');
   streamFile('/dashboard.html', 'text/html');
-  
+
   function streamFile(url, type) {
     if (req.url == url) {
       res.writeHead(200, { 'content-type': type});
       fs.createReadStream('.' + url).pipe(res);
     }
   }
-});
-
-server.listen(8000);
-
-var io = engine.attach(server);
-
-io.on('connection', function(socket) {
-  ee.on('progress', function(status) {
-    socket.send(JSON.stringify(status));
-  });
-});
+}
 
