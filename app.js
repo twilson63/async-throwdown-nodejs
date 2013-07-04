4a9daff08398d39b13fdf42b2f4af15ba9ca512a
Tom Wilson <tom@jackhq.com>
2013-07-04 19:54:45 -0400

    added cluster

--- app.js
+++ app.js
@@ -16,35 +16,49 @@ var http   = require('http'),
     get: { '/dashboard': function(req, res) { 
       filed(path.join(__dirname, 'public', 'index.html'))
         .pipe(res); 
+      }
     }
-  }
-};
+  },
+  cluster = require('cluster');
 
 http.globalAgent.maxSockets = MAX_CONNECTIONS;
+var numCPUs = require('os').cpus().length;
 
-/* create server and handle request */
-var server = http.createServer(function(req, res) {
-  /* look for matched route in route obj */
-  var fn = router[req.method.toLowerCase()][req.url];
-  /* if found then call the route function */
-  if (fn) { return fn(req, res); }
-  /* otherwise pipe to filed and serve static files */
-  req.pipe(filed(path.join(__dirname, 'public', req.url))).pipe(res);
-  // if error shutdown server and restart
-  req.on('error', function(err) {
-    console.log(err);
-    server.close();
-    var endApp = function() { process.exit(1); }
-    // wait 30sec for all connections to close
-    setTimeout(endApp, 30000).unref();
-  });
-}).listen(process.env.PORT || 8000, function() {
-  console.log('Server is Listening...(CTRL-C to close)');
-});
+if (cluster.isMaster) {
+  // Fork workers.
+  console.log(numCPUs);
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// setup websockets to pass progress
-engine.attach(server).on('connection', function(socket) {
-  ee.on('progress', function(status) {
-    socket.send(JSON.stringify(status));
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
   });
-});
+} else {
+  /* create server and handle request */
+  var server = http.createServer(function(req, res) {
+    /* look for matched route in route obj */
+    var fn = router[req.method.toLowerCase()][req.url];
+    /* if found then call the route function */
+    if (fn) { return fn(req, res); }
+    /* otherwise pipe to filed and serve static files */
+    req.pipe(filed(path.join(__dirname, 'public', req.url))).pipe(res);
+    // if error shutdown server and restart
+    req.on('error', function(err) {
+      console.log(err);
+      server.close();
+      var endApp = function() { process.exit(1); }
+      // wait 30sec for all connections to close
+      setTimeout(endApp, 30000).unref();
+    });
+  }).listen(process.env.PORT || 8000, function() {
+    console.log('Server is Listening...(CTRL-C to close)');
+  });
+
+  // setup websockets to pass progress
+  engine.attach(server).on('connection', function(socket) {
+    ee.on('progress', function(status) {
+      socket.send(JSON.stringify(status));
+    });
+  });
+}
